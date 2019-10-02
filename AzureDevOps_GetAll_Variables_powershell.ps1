8bdc307d8fef29166ec93446bd7d17c3a0f84164
Noman Sadiq <nomi.cs786@gmail.com>
2019-10-02 12:03:51 +0400

    updated comments

--- AzureDevOps_GetAll_Variables_powershell.ps1
+++ AzureDevOps_GetAll_Variables_powershell.ps1
@@ -1,3 +1,8 @@
 
 
+#this is use to get all variables which having in Azure DevOps Pipelines 
 Get-ChildItem Env:
+
+
+#Importan comment 
+# do not use reserverd workd "path" as variable which causing issue
